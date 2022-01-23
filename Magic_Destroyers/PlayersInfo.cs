f53c9307ae67474ef4a44361d1f120c95b6d1a99
seymseym <seym.kurt97@gmail.com>
2022-01-23 23:28:55 +0300

    Added a static class for getting the informations from the players

---
+++ Magic_Destroyers/PlayersInfo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Magic_Destroyers
+{
+    public static class PlayersInfo
+    {
+        private static string playersInfoDirectory = "";
+        private static string[,] fullInfo;
+        private static int[] scores;
+        private static int[] levels;
+
+        static PlayersInfo() // Constructor
+        {
+
+        }
+        //public static void Initialize() // Could also use this instead of a constructor
+        //{
+
+        //}
+        public static void Save()
+        {
+
+        }
+        public static void UpdateFullInfo()
+        {
+
+        }
+        public static void RetrieveFullInfo()
+        {
+
+        }
+        public static void PrintFullInfo()
+        {
+
+        }
+        public static void EraseFullInfo()
+        {
+
+        }
+        public static void UpdateScores()
+        {
+
+        }
+        public static void RetrieveScores()
+        {
+
+        }
+        public static void PrintScores()
+        {
+
+        }
+        public static void EraseScores()
+        {
+
+        }
+        public static void UpdateLevels()
+        {
+
+        }
+        public static void RetrieveLevels()
+        {
+
+        }
+        public static void PrintLevels()
+        {
+
+        }
+        public static void EraseLevels()
+        {
+
+        }
+
+    }
+}
